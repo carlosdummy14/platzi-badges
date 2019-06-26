db0831942c6e04015f37183c2cecc1cbe3856e8e
support <carlosdummy14@gmail.com>
2019-06-26 17:54:40 -0500

    Tabajo con JSX y createElement

--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hello, Platzi Badges!';
-
-// const container = document.getElementById('app');
-
-// container.appendChild(element);
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-
-// const element = <h1>Hello, Platzi Badges!</h1>;
-
-// const container = document.getElementById('app');
-
-// // ReactDOM.render(__qué__, __dónde__);
-// ReactDOM.render(element, container);
-
 import React from "react"; // analogo a createElement
 import ReactDOM from "react-dom"; // analogo a appendChild
 
-const element = <h1>Hola, Amigos de React con React!!!</h1>;
+// const jsx = <h1>Hola, Amigos de React con React!!!</h1>;
+// const element = React.createElement(
+//   "a",
+//   { href: "https://platzi.com" },
+//   "Ir a platzi"
+// );
+// const name = "Angela";
+// const sum = () => 3 + 3;
+// const element = React.createElement("h1", {}, `Hola, soy ${name}`);
+// const jsx = <h1>Hola soy {sum()} con JSX</h1>;
+
+const jsx = (
+  <div>
+    <h1>Hola, soy Papi</h1>
+    <p>Soy ingeniero frontend.</p>
+  </div>
+);
+
+const element = React.createElement(
+  "div",
+  {},
+  React.createElement("h1", {}, "Hola, soy Papi con createElement"),
+  React.createElement("p", {}, "Soy ingeniero frontend.")
+);
 
 const container = document.getElementById("app");
 
