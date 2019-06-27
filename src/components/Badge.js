f81c51a90a6e57a6af5cc0ca8af8674ecad60df3
support <carlosdummy14@gmail.com>
2019-06-27 17:32:13 -0500

    Trabajamos con Props

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -16,7 +16,7 @@ class Badge extends React.Component {
         <div className="Badge__section-name">
           <img
             className="Badge__avatar"
-            src="https://www.gravatar.com/avatar?d=identicon"
+            src={this.props.avatarUrl}
             alt="Avatar"
           />
           <h1>
@@ -25,8 +25,8 @@ class Badge extends React.Component {
         </div>
 
         <div className="Badge__section-info">
-          <h3>Frontend Enginee</h3>
-          <div>@mitwitteres</div>
+          <h3>{this.props.jobTitle}</h3>
+          <div>@{this.props.twitter}</div>
         </div>
 
         <div className="Badge__footer">#conferencia</div>
--- src/index.js
+++ src/index.js
@@ -8,4 +8,13 @@ import Badge from "./components/Badge";
 const container = document.getElementById("app");
 
 // ReactDOM.render( que queremos renderizar, donde lo vamos a renderizar)
-ReactDOM.render(<Badge />, container);
+ReactDOM.render(
+  <Badge
+    firstName="Ramón"
+    lastName="López"
+    avatarUrl="https://www.gravatar.com/avatar?d=identicon"
+    jobTitle="Kickass senior dev"
+    twitter="chilangomagic"
+  />,
+  container
+);
