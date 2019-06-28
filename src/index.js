7d38fa77c58eeaaf1e73424dfece02e43519aa5b
support <carlosdummy14@gmail.com>
2019-06-28 17:25:47 -0500

    Creamos la primer pagina con Navbar, hero y Badge

--- src/index.js
+++ src/index.js
@@ -3,18 +3,9 @@ import ReactDOM from "react-dom"; // analogo a appendChild
 import "bootstrap/dist/css/bootstrap.css";
 
 import "./global.css";
-import Badge from "./components/Badge";
+import BadgeNew from "./pages/BadgeNew";
 
 const container = document.getElementById("app");
 
 // ReactDOM.render( que queremos renderizar, donde lo vamos a renderizar)
-ReactDOM.render(
-  <Badge
-    firstName="Ramón"
-    lastName="López"
-    avatarUrl="https://www.gravatar.com/avatar?d=identicon"
-    jobTitle="Kickass senior dev"
-    twitter="chilangomagic"
-  />,
-  container
-);
+ReactDOM.render(<BadgeNew />, container);
