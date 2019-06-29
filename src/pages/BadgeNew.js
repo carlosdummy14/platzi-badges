39b6cc278d53a770de4a73cf3649024b591b66c9
support <carlosdummy14@gmail.com>
2019-06-29 15:44:17 -0500

    Levantamos el estado de los componentes y pasamos estado entre BadgeForm y BadgeNew, y afectamos a otro componente Badge con esos datos

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -7,6 +7,24 @@ import Badge from "../components/Badge";
 import BadgeForm from "../components/BadgeForm";
 
 class BadgeNew extends React.Component {
+  state = {
+    form: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      jobTitle: "",
+      twitter: ""
+    }
+  };
+
+  handleChange = e => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
   render() {
     return (
       <div>
@@ -20,15 +38,19 @@ class BadgeNew extends React.Component {
           <div className="row">
             <div className="col-6">
               <Badge
-                firstName="Carlos"
-                lastName="López"
-                jobTitle="Frontend Engineer"
-                twitter="chilangomagic"
+                firstName={this.state.form.firstName}
+                lastName={this.state.form.lastName}
+                jobTitle={this.state.form.jobTitle}
+                twitter={this.state.form.twitter}
+                email={this.state.form.email}
                 avatarUrl="https://www.gravatar.com/avatar?d=identicon"
               />
             </div>
             <div className="col-6">
-              <BadgeForm />
+              <BadgeForm
+                onChange={this.handleChange}
+                formValues={this.state.form}
+              />
             </div>
           </div>
         </div>
