32403b4a42e3bc2151b254988f4a0b6aa88d8d69
support <carlosdummy14@gmail.com>
2019-07-05 18:19:41 -0500

    Creamos Single Page App con React Router

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import ReactDOM from "react-dom"; // analogo a appendChild
 import "bootstrap/dist/css/bootstrap.css";
 
 import "./global.css";
-import BadgeNew from "./pages/BadgeNew";
-import Badges from "./pages/Badges";
+import App from "./components/App";
 
 const container = document.getElementById("app");
 
 // ReactDOM.render( que queremos renderizar, donde lo vamos a renderizar)
-ReactDOM.render(<Badges />, container);
+ReactDOM.render(<App />, container);
