6502fcfe4aa00c5bfe1b8db146ea43e854db3f97
support <carlosdummy14@gmail.com>
2019-07-30 14:32:34 -0500

    Updates

---
+++ precache-manifest.12f07e31a35b3ad6c928bf2d1bb1315c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/platzi-badges/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "bbe78c75c6ada52940a2",
+    "url": "/platzi-badges/static/css/main.818c5b8e.chunk.css"
+  },
+  {
+    "revision": "0f7117b146aebd482afe",
+    "url": "/platzi-badges/static/js/runtime~main.0f7117b1.js"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/platzi-badges/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "b26109ca21dd86b83f38",
+    "url": "/platzi-badges/static/js/2.b26109ca.chunk.js"
+  },
+  {
+    "revision": "bbe78c75c6ada52940a2",
+    "url": "/platzi-badges/static/js/main.bbe78c75.chunk.js"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/platzi-badges/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/platzi-badges/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/platzi-badges/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "b26109ca21dd86b83f38",
+    "url": "/platzi-badges/static/css/2.d826f1f4.chunk.css"
+  },
+  {
+    "revision": "694a231e62f565768818ff8cb099e457",
+    "url": "/platzi-badges/index.html"
+  }
+];
